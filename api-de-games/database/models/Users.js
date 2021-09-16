59845ee45ff0189e922e21974dab1dbb94c92886
ShankCGC <shankcgc@gmail.com>
2021-09-16 19:31:41 -0300

    📕 Adding Users Model

---
+++ api-de-games/database/models/Users.js
@@ -0,0 +1,21 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const connection = require('../index')
+
+const Users = connection.define('users', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+console.log('\x1b[36m[TABELA]\x1b[0m Tabela \x1b[36mUsers\x1b[0m carregada com sucesso.')
+
+module.exports = Users
